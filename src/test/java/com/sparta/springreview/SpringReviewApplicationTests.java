a1e7de414ec41da1c571bea0987a5f7a5f24ffed
Inseo Kim <itsinseo@gmail.com>
2023-07-26 09:09:25 +0900

    init commit

---
+++ src/test/java/com/sparta/springreview/SpringReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
